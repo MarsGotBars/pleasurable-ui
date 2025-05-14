71059ad014696669acf9598bd4843aa394258dda
Marcin Magdziak <marcinm@live.nl>
2025-05-14 18:12:47 +0200

    viewtransition voor details
    checkt voor view transition support, hiernaast doet dit script vrij weinig, heb issue #47 aangemaakt voor het geval ik (of iemand anders) er nog aan komt om een no-viewtransition versie te maken

---
+++ public/scripts/transitionDetails.js
@@ -0,0 +1,18 @@
+// Als ViewTransition mogelijk is
+if (document.startViewTransition) {
+  // Selecteer het details element in de header
+  const headerDetails = document.querySelector("header nav details");
+  // Onthou de state van het details element (open/dicht)
+  let openDetail = false;
+  // Op click
+  headerDetails.addEventListener("click", (e) => {
+    // Start transitie
+    document.startViewTransition(() => {
+      // invert openDetail om details open/dicht te zetten
+      openDetail = !openDetail;
+      headerDetails.open = openDetail;
+    });
+    // Prevent default aan het einde in het geval dat er iets fout gaat
+    e.preventDefault();
+  });
+}
