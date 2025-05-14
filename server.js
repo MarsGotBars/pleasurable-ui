e03ce62023a41b63cbfe7b828982e238a047ccbd
Marcin Magdziak <marcinm@live.nl>
2025-05-14 12:37:12 +0200

    Update server.js
    added extensive way of collecting correct exercise data with plenty of explanatory comments

--- server.js
+++ server.js
@@ -26,11 +26,23 @@ app.listen(app.get('port'), function () {
 
 // taken pagina ophalen 
 app.get('/task/:id', async function (request, response) {       // Je haalt de id op die uit de filter (<a> van task.lquid) komt. 
-  const task = request.params.id;                               // Je maakt een variabele aan voor de opgevraagde id
-  const taskResponse = await fetch(`https://fdnd-agency.directus.app/items/dropandheal_task/?fields=*.*.*&filter={"id":"${task}"}&limit=1`) // De variable kan je in de link terug laten komen + door de fields komen taken en opdrachten samen in een API.
-  const taskResponseJSON = await taskResponse.json()            // Je zet de data om in JSON
+  const taskId = request.params.id - 1;                               // Je maakt een variabele aan voor de opgevraagde id
+  const taskResponse = await fetch(`https://fdnd-agency.directus.app/items/dropandheal_task`) // De variable kan je in de link terug laten komen + door de fields komen taken en opdrachten samen in een API.
+  const {data: taskResponseJSON} = await taskResponse.json() // Je zet de data om in JSON
 
-  response.render('task.liquid', {task: taskResponseJSON.data?.[0] || [] }) 
+  // Iteratief halen we alle plaatjes op, we zetten -1 omdat de id in directus start op 1 en niet op 0
+  const exerciseData = taskResponseJSON[taskId].exercise.map( async (exerciseCount) => {
+    
+    // exerciseCount is het id van de exercise die we ophalen
+    const imgData = await fetch(`https://fdnd-agency.directus.app/items/dropandheal_exercise?filter[id][_eq]=${exerciseCount}&fields=*,image.width,image.height,image.id`)
+
+    // destructureren - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring
+    const {data: imgJSON} = await imgData.json();
+    return imgJSON[0]
+  });
+
+  // we returnern allTasks, en taskId om erachter te komen welke task we willen.
+  response.render('task.liquid', {allTasks: taskResponseJSON || [], taskId, exerciseData }) 
 })
 
 
